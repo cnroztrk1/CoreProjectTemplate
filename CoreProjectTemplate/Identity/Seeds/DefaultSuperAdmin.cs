f443cf37df8e4a906bae0b02cd140fda8e550aea
Caner Öztürk <cnroztrk1@gmail.com>
2024-07-30 14:07:43 +0300

    Add project files.

---
+++ CoreProjectTemplate/Identity/Seeds/DefaultSuperAdmin.cs
@@ -0,0 +1,37 @@
+﻿using Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Models.Enums;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.Seeds
+{
+    public static class DefaultSuperAdmin
+    {
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager)
+        {
+            //Seed Default User
+            var defaultUser = new ApplicationUser
+            {
+                UserName = "cnroztrk1",
+                Email = "cnroztrk1@gmail.com",
+                FirstName = "Caner",
+                LastName = "Öztürk",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true
+            };
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultUser, "123qwe");
+                    await userManager.AddToRoleAsync(defaultUser, Roles.Basic.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Roles.Moderator.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Roles.Admin.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, Roles.SuperAdmin.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ CoreProjectTemplate/WebApi/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using Data.Mongo.Collections;
+using Identity.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models.DTOs.Account;
+using Services.Interfaces;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+        private readonly ILoginLogService _loginLogService;
+        public AccountController(IAccountService accountService, ILoginLogService loginLogService)
+        {
+            _accountService = accountService;
+            _loginLogService = loginLogService;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> AuthenticateAsync(AuthenticationRequest request)
+        {
+            //auth
+            var result = await _accountService.AuthenticateAsync(request);
+            if (result.Errors == null || !result.Errors.Any())
+            {
+                //mongo usage example
+                LoginLog log = new LoginLog()
+                {
+                    LoginTime = DateTime.Now,
+                    UserEmail = request.Email
+                };
+                await _loginLogService.Add(log);
+            }
+            return Ok(result);
+        }
+
+        [HttpPost("refreshtoken")]
+        public async Task<IActionResult> RefreshTokenAsync(RefreshTokenRequest request)
+        {
+            return Ok(await _accountService.RefreshTokenAsync(request));
+        }
+        private string GenerateIPAddress()
+        {
+            if (Request.Headers.ContainsKey("X-Forwarded-For"))
+                return Request.Headers["X-Forwarded-For"];
+            else
+                return HttpContext.Connection.RemoteIpAddress.MapToIPv4().ToString();
+        }
+    }
+}
